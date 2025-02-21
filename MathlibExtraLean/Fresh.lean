5b3f90eefb3468e9806cfbd2c11c001eeda122d5
Patrick Thomas <pthomas505@gmail.com>
2025-02-21 13:43:17 -0800

    Updates.

--- MathlibExtraLean/Fresh.lean
+++ MathlibExtraLean/Fresh.lean
@@ -6,7 +6,7 @@ set_option autoImplicit false
 
 
 /--
-  finset_string_max_len xs := The length of the longest string in the finite set of strings xs or 0 if the set is empty.
+  `finset_string_max_len xs` := The length of the longest string in the finite set of strings `xs` or 0 if the set is empty.
 -/
 def finset_string_max_len :
   Finset String → ℕ :=
@@ -21,26 +21,27 @@ lemma finset_string_max_len_mem
   by
   induction xs using Finset.induction_on
   case empty =>
-    simp at h1
+    simp only [Finset.not_mem_empty] at h1
   case insert hd tl a1 ih =>
-    simp at h1
+    simp only [Finset.mem_insert] at h1
 
     cases h1
     case inl c1 =>
-      subst c1
-      simp only [finset_string_max_len]
-      simp
+      rewrite [c1]
+      unfold finset_string_max_len
+      simp only [Finset.fold_insert_idem, le_sup_left]
     case inr c1 =>
       simp only [finset_string_max_len] at ih
 
       simp only [finset_string_max_len]
-      simp
+      simp only [Finset.fold_insert_idem, le_sup_iff]
       right
-      exact ih c1
+      apply ih
+      exact c1
 
 
 /--
-  fresh x c xs := If the variable name x is not a member of the finite set of variable names xs then x is returned. If x is a member of xs then the character c is repeatedly appended to x until the resulting variable name is not a member of xs. The resulting variable name is then returned.
+  `fresh x c xs` := If the string `x` is not a member of the finite set of strings `xs` then `x` is returned. If `x` is a member of `xs` then the character `c` is repeatedly appended to `x` until the resulting string is not a member of `xs`. The resulting string is then returned.
 -/
 def fresh
   (x : String)
@@ -52,11 +53,11 @@ def fresh
     have : finset_string_max_len xs - String.length x < finset_string_max_len xs + 1 - String.length x :=
       by
       obtain s1 := finset_string_max_len_mem x xs h
-      simp only [tsub_lt_tsub_iff_right s1]
-      simp
+      apply Nat.sub_lt_sub_right s1
+      apply lt_add_one
   fresh (x ++ c.toString) c xs
   else x
-  termination_by finset_string_max_len xs + 1 - x.length
+  termination_by (finset_string_max_len xs) + 1 - x.length
 
 
 lemma fresh_not_mem
@@ -69,19 +70,17 @@ lemma fresh_not_mem
   have : finset_string_max_len xs - String.length x < finset_string_max_len xs + 1 - String.length x :=
     by
     obtain s1 := finset_string_max_len_mem x xs h
-    simp only [tsub_lt_tsub_iff_right s1]
-    simp
+    apply Nat.sub_lt_sub_right s1
+    apply lt_add_one
   by
     unfold fresh
-    simp
-    simp only [if_pos h]
+    split_ifs
     apply fresh_not_mem
   else by
     unfold fresh
-    simp
-    simp [if_neg h]
+    split_ifs
     exact h
-  termination_by finset_string_max_len xs + 1 - x.length
+  termination_by (finset_string_max_len xs) + 1 - x.length
 
 
 #lint
