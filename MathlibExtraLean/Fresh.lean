ff2d45c17cb9e9a1abc82e21bb9f89e3c8eb7992
Patrick Thomas <pthomas505@gmail.com>
2026-08-26 21:03:45 -0700

    Updates.

--- MathlibExtraLean/Fresh.lean
+++ MathlibExtraLean/Fresh.lean
@@ -8,7 +8,7 @@ set_option linter.style.longLine false
 
 
 /--
-  `finset_string_max_len xs` := The length of the longest string in the finite set of strings `xs` or 0 if the set is empty.
+  `finset_string_max_len css` := The length of the longest string in the finite set of strings `css` or 0 if the set is empty.
 -/
 def finset_string_max_len :
   Finset String → ℕ :=
@@ -16,15 +16,15 @@ def finset_string_max_len :
 
 
 theorem finset_string_max_len_mem
-  (x : String)
-  (xs : Finset String)
-  (h1 : x ∈ xs) :
-  x.length <= finset_string_max_len xs :=
+  (cs : String)
+  (css : Finset String)
+  (h1 : cs ∈ css) :
+  cs.length ≤ finset_string_max_len css :=
   by
-  induction xs using Finset.induction_on
+  induction css using Finset.induction_on
   case empty =>
     simp only [Finset.notMem_empty] at h1
-  case insert hd tl a1 ih =>
+  case insert hd tl ih_1 ih_2 =>
     simp only [Finset.mem_insert] at h1
 
     cases h1
@@ -33,45 +33,45 @@ theorem finset_string_max_len_mem
       unfold finset_string_max_len
       simp only [Finset.fold_insert_idem, le_sup_left]
     case inr h1 =>
-      simp only [finset_string_max_len] at ih
+      simp only [finset_string_max_len] at ih_2
 
       simp only [finset_string_max_len]
       simp only [Finset.fold_insert_idem, le_sup_iff]
       right
-      apply ih
+      apply ih_2
       exact h1
 
 
 /--
-  `fresh x c xs` := If the string `x` is not a member of the finite set of strings `xs` then `x` is returned. If `x` is a member of `xs` then the character `c` is repeatedly appended to `x` until the resulting string is not a member of `xs`. The resulting string is then returned.
+  `fresh cs c css` := If the string `cs` is not a member of the finite set of strings `css` then `cs` is returned. If `cs` is a member of `css` then the character `c` is repeatedly appended to `cs` until the resulting string is not a member of `css`. The resulting string is then returned.
 -/
 def fresh
-  (x : String)
+  (cs : String)
   (c : Char)
-  (xs : Finset String) :
+  (css : Finset String) :
   String :=
-  if h : x ∈ xs
+  if h : cs ∈ css
   then
-    have : finset_string_max_len xs - String.length x < finset_string_max_len xs + 1 - String.length x :=
+    have : finset_string_max_len css - String.length cs < finset_string_max_len css + 1 - String.length cs :=
       by
-      obtain s1 := finset_string_max_len_mem x xs h
+      obtain s1 := finset_string_max_len_mem cs css h
       apply Nat.sub_lt_sub_right s1
       apply lt_add_one
-  fresh (x ++ c.toString) c xs
-  else x
-  termination_by (finset_string_max_len xs) + 1 - x.length
+  fresh (cs ++ c.toString) c css
+  else cs
+  termination_by (finset_string_max_len css) + 1 - cs.length
 
 
 theorem fresh_not_mem
-  (x : String)
+  (cs : String)
   (c : Char)
-  (xs : Finset String) :
-  fresh x c xs ∉ xs :=
-  if h : x ∈ xs
+  (css : Finset String) :
+  fresh cs c css ∉ css :=
+  if h : cs ∈ css
   then
-  have : finset_string_max_len xs - String.length x < finset_string_max_len xs + 1 - String.length x :=
+  have : finset_string_max_len css - String.length cs < finset_string_max_len css + 1 - String.length cs :=
     by
-    obtain s1 := finset_string_max_len_mem x xs h
+    obtain s1 := finset_string_max_len_mem cs css h
     apply Nat.sub_lt_sub_right s1
     apply lt_add_one
   by
@@ -88,4 +88,4 @@ theorem fresh_not_mem
       contradiction
     case isFalse c1 =>
       exact h
-  termination_by (finset_string_max_len xs) + 1 - x.length
+  termination_by (finset_string_max_len css) + 1 - cs.length
